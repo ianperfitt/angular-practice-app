fb2c9a6614f08539e91d14807870721c11f505a0
Ian Perfitt <iancp@protonnmail.com>
2024-07-17 14:17:21 -0500

    Refreshed concept of union types and interfaces in typescript.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,22 @@
 import { Component, Output } from '@angular/core';
 
+interface User {
+  name: string;
+  id: number;
+};
+
+class UserAccount {
+  name: string;
+  id: number;
+
+  constructor(name: string, id: number) {
+    this.name = name;
+    this.id = id;
+  }
+}
+
+type MyType = string | number;
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -9,6 +26,13 @@ export class AppComponent {
   title = 'interpolated text';
   itemImageUrl = '../assets/me.jpeg'
   isUnchanged = true;
+  user!: User;
+
+  constructor() {
+    this.user = new UserAccount("Ian", 0);
+    console.log(typeof foo1("hello"));
+    console.log(typeof foo1(2000));
+  }
 
   @Output()
   donkeyTwo = 'This text is property bound';
@@ -17,4 +41,11 @@ export class AppComponent {
     console.log("HELLO !");
   }
 
+}
+
+function foo1(arg0: MyType): MyType {
+  if (typeof arg0 == "string") {
+    return "string";
+  }
+  return 100;
 }
